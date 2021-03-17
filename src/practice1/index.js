2cbd9a64f66cea0c184c76d0eea05a4ef93368cf
Xiran Xie <xiran.xie@thoughtworks.com>
2021-03-17 22:44:35 +0800

    pass three practices

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,5 @@
+import { ADDRESS, PORT, PATH, REGION } from "./constant";
+import { getURL } from "./utils";
 
+export const url = getURL(ADDRESS, PORT, PATH);
+export const region = REGION;
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,22 @@
-export const getPoetry
+// export const getPoetry = () => {
+//   return fetch("https://v1.jinrishici.com/all.json", {
+//     method: "GET",
+//   })
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((result) => {
+//       return [result.origin, result.author, result.content];
+//     });
+// };
+
+// eslint-disable-next-line consistent-return
+export const getPoetry = async () => {
+  const url = "https://v1.jinrishici.com/all.json";
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return [result.origin, result.author, result.content];
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
+};
